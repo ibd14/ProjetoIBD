bfaac972ca27ade49d224d5da8159f5476894874
Isadora <isadora201565@gmail.com>
2020-02-07 10:15:53 -0300

    meu primeiro COMMIT

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Teste</title>
+</head>
+
+<body>
+
+	Olá, bom dia!</br>
+    Aula Programação Web</br></br>
+    
+    Bárbara, Derlange e Isadora
+
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</body>
+</html>
