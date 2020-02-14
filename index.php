da4bf90d07f15a665fdb9b7e879ae3b2a4606814
ibd14 <isadora201565@gmail.com>
2020-02-14 09:41:23 -0300

    Revisão GitHub

--- index.php
+++ index.php
@@ -1,20 +1,15 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Teste</title>
+    <title>Aula Prog. WEB</title>
 </head>
 
 <body>
 
-	Olá, bom dia!</br>
-    Aula Programação Web</br></br>
-    
-    Bárbara, Derlange e Isadora
+    <h2>Utilizando GitHub</h2>
 
-
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<p>&nbsp;</p>
 </body>
+
+
+
+
 </html>
